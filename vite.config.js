35fee98a55584b8ccb4e032ec6cbd8195df24053
lydia-cmyk <graciasalavidawn@gmail.com>
2025-08-21 12:09:52 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/be-myself-get-mine/',
   plugins: [react()],
+  build: { sourcemap: true }        // ← 추가 (에러 위치가 원본 코드로 보임)
 })
